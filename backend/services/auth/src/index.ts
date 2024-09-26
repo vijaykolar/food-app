9a2e2fa7a0d0aeb99c07efa85b26b9bba4b6ea22
Vijay Kolar <vijayikolar@gmail.com>
2024-09-26 13:35:49 +0530

    arranged imports

--- backend/services/auth/src/index.ts
+++ backend/services/auth/src/index.ts
@@ -1,17 +1,22 @@
-import * as dotenv from "dotenv";
+import express from 'express';
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+import { usersRouter } from './routes/users';
+
 dotenv.config();
 const PORT = process.env.PORT || 3000;
-import express from "express";
-import cors from "cors";
-
-import { usersRouter } from "./routes/users";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/v1", usersRouter);
+console.log('Hello World');
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.use('/api/v1', usersRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
